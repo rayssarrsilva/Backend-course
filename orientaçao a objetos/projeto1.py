f2bcc6f62c4b97bb91dc18e6774ed9dc45f6ed6d
rayy0ssa_06 <rayssarrsilva1@gmail.com>
2024-04-12 16:42:19 -0300

    criando uma classe de exemplo

---
+++ orientaçao a objetos/projeto1.py
@@ -0,0 +1,6 @@
+#declarando classes
+#No Python, todas as classes devem, por boas práticas, possuir nomes que comecem com letra maiúscula e
+#caso sejam compostos, a primeira letra de cada palavra deve ser maiúscula, o que chamamos de formato CamelCase.
+
+class PessoaFisica():
+    #atributos e metodos
