2a232b1529447d9e596fefea9ab38f855e243433
rayssa roberta <rayssarrsilva1@gmail.com>
2024-04-13 14:51:44 -0300

    funçoes, orientaçao a objetos

--- orientaçao a objetos/projeto1.py
+++ orientaçao a objetos/projeto1.py
@@ -10,6 +10,7 @@ class pessoa():
         self.cpf  = cpf
 
 class pessoa2():
+    #construtores padrao tem init o resto nao precisa
     def __init__(self, idade, signo):
         self.idade = idade
         self.signo = signo
@@ -31,3 +32,58 @@ print(objeto1.nome)
 objeto1.sexo = "F"
 print(objeto1.sexo)
 
+#declarando métodos
+class Pessoa:
+    #atributos do construtor
+    def __init__(self, nome, sexo,cpf,ativo):
+        self.nome = nome
+        self.sexo = sexo
+        self.cpf = cpf
+        self.ativo = ativo
+
+    #metodo 
+    def desativar(self):
+        self.ativo = False 
+        print("A pessoa foi desativada com sucesso")
+
+#instanciando
+if __name__ == "__main__":
+    pessoaA = Pessoa("ray","F","123456", True)
+    #ao puxar o metodo com o objeto, caso ele ja possua print nao é necessario chamar print.
+    pessoaA.desativar()
+
+#declarando propriedades
+
+ # Utilizando properties
+    pessoaA.nome = "José"
+    print(pessoaA.nome)
+
+# Ao chamar uma função, indicamos somente os valores com os quais ela vai operar
+def soma(a, b):
+    resultado = a+b
+    return resultado
+
+print(soma(123412,5234))
+#Se adicionarmos um parâmetro na criaçao da função, ao chamá-la teremos que inserir um argumento
+def ola_mundo(nome):
+    print("bem vindo ao terminal do python",nome,"!")
+
+ola_mundo("rayssa")
+
+#parametro Nomeado (não importa a ordem)
+print(soma(a=12, b=14))
+
+#parametro Posicional (a ordem importa)
+print(soma(123123, 345345))
+
+#para adicionar um parametro que pode ser chamado infinitas vezes adicione asterisco
+def cadastro(*nome):
+    print("ola",nome,"!")
+
+cadastro("rayssa", "luiza", "clara", "julia", "ana")
+
+def calculo(a,b):
+    resultado = a - b
+    return resultado #usado geralmente pra calculos
+
+calculo(8123, 123)
